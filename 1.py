30785382817ad3162f2bfe94d4885244f43a4a99
ybiren <37359767+ybiren@users.noreply.github.com>
2024-10-26 10:25:58 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,12 @@
 from datetime import datetime
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import WebDriverException
 
 fileToWrite = open("dd.txt", "a", encoding="utf-8")
 now = datetime.now()
