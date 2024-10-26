20411a074ff033b0f1b078d47e10067ab0b9730f
ybiren <37359767+ybiren@users.noreply.github.com>
2024-10-26 10:28:27 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,8 +8,28 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
 
+def initSeleniumDriver():
+    # Setup Chrome options to run in headless mode
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_argument("--log-level=3") # Fatal errors only
+    #options.add_argument("--headless")
+    options.add_argument("--headless=new")
+    #options.add_argument("--disable-gpu")
+    options.add_argument("--window-position=-2400,-2400")
+  
+    #driver_path = r'C:\Users\yossi\.wdm\drivers\chromedriver\win64\127.0.6533.72\chromedriver-win32\chromedriver.exe'
+    # Setup WebDriver
+    #return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return webdriver.Chrome(options=options)
+
+
+
 fileToWrite = open("dd.txt", "a", encoding="utf-8")
 now = datetime.now()
 str = now.strftime('%Y-%m-%d %H:%M:%S')
 fileToWrite.write(str)  
 fileToWrite.close()
+
+driver = initSeleniumDriver()
+
