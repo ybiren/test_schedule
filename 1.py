21da21b02260dd9e10caa212a866cf5cabe61b3e
ybiren <37359767+ybiren@users.noreply.github.com>
2024-10-26 11:24:52 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
 
@@ -18,9 +17,6 @@ def initSeleniumDriver():
     #options.add_argument("--disable-gpu")
     options.add_argument("--window-position=-2400,-2400")
   
-    #driver_path = r'C:\Users\yossi\.wdm\drivers\chromedriver\win64\127.0.6533.72\chromedriver-win32\chromedriver.exe'
-    # Setup WebDriver
-    #return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return webdriver.Chrome(options=options)
 
 
