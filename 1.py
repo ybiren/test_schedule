3879e4da6aa1ce4bf6ed46e47e6ef9661ab10ddf
ybiren <37359767+ybiren@users.noreply.github.com>
2024-10-27 05:25:46 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
+import os
 
 def initSeleniumDriver():
     # Setup Chrome options to run in headless mode
